14da080007c91fc489f7b0a7381e1083e501a3ea
Knightking-9274 <119316323+Knightking-9274@users.noreply.github.com>
2024-06-27 15:40:03 +0530

    Change in jpql query

--- src/main/java/com/nebulamart/backend/repositories/OrderRepository.java
+++ src/main/java/com/nebulamart/backend/repositories/OrderRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 import com.nebulamart.backend.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query("select o from Order o where o.userId = :userId and(o.orderStatus=placed or o.orderStatus=confirmed or o.orderStatus=shipped or o.orderStatus=delivered)")
+    @Query("select o from Order o where o.user.id = :userId and(o.orderStatus=placed or o.orderStatus=confirmed or o.orderStatus=shipped or o.orderStatus=delivered)")
     public List<Order>getUsersOrders(@Param("userId")Long userId);
 }
